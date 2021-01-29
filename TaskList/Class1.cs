0f0721a4f513429464d78e4372890ccc82461e09
Wendi Magee <wendimagee@gmail.com>
2021-01-29 09:58:18 -0500

    Add project files.

---
+++ TaskList/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskList
+{
+    class Task
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string DueDate { get; set; }
+        public bool Status { get; set; }
+
+        public Task(string Name, string Description, string DueDate, bool Status)
+        {
+            this.Name = Name;
+            this.Description = Description;
+            this.DueDate = DueDate;
+            this.Status = Status;
+        }
+    }
+}
---
+++ TaskList/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TaskList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Task Manager");
+            bool goAgain = true;
+
+            while (goAgain = true)
+            {
+                Console.WriteLine("1. List Tasks");
+                Console.WriteLine("2. Add Task");
+                Console.WriteLine("3. Delete Task");
+                Console.WriteLine("4. Mark task complete");
+                Console.WriteLine("5. Quit");
+                Console.WriteLine("What would you like to do?");
+                try
+                {
+                    string input = Console.ReadLine()
+                    string userChoice = input.ToLower().Trim();
+
+                    if(userChoice == "5" || userChoice == "Quit")
+                    {
+                        goAgain = false;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter a number 1-5");
+                }
+            }
+
+        }
+    }
+}

8ca83aadc79feb5bc8526f4347703d708149d727
Wendi Magee <wendimagee@gmail.com>
2021-02-10 11:16:31 -0500

    After turning in this assignment, my teacher gave me notes saying I should make the Program.cs file cleaner by refactoring. I put the lengthy tasks into methods at the bottom of Program.cs.

--- TaskList/Program.cs
+++ TaskList/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
+
 namespace TaskList
 {
     class Program
@@ -35,139 +36,19 @@ namespace TaskList
                     }
                     if (userChoice == "2" || userChoice == "add task")
                     {
-                        Console.WriteLine("Please enter the team member's name: ");
-                        string taskName = Console.ReadLine();
-                        Console.WriteLine("Please enter the task description: ");
-                        string taskDescription = Console.ReadLine();
-                        Console.WriteLine("Please enter the due date: ");
-                        string dueDate = Console.ReadLine();
-                        tasks.Add(new Job($"{taskName}", $"{taskDescription}", $"{dueDate}", false));
-
+                        AddTask(tasks);
                     }
                     if (userChoice == "3" || userChoice == "delete task")
                     {
-                        foreach (Job t in tasks)
-                        {
-                            Console.WriteLine($"    {tasks.IndexOf(t) + 1}.Team Member: {t.Name}");
-                            Console.WriteLine($"    Task Description: {t.Description}");
-                            Console.WriteLine($"    Task Due Date: {t.DueDate}");
-                            if (t.Status == true)
-                            {
-                                Console.WriteLine("    Task Status: Complete");
-                            }
-                            else
-                            {
-                                Console.WriteLine("    Task Status: Incomplete");
-                            }
-                        }
-                        Console.WriteLine("Which task would you like to delete?");
-                        string input2 = Console.ReadLine();
-                        int userChoice2 = Int32.Parse(input2) - 1;
-                        //check if userchoice2 is in index
-                        if (userChoice2 > tasks.Count() || userChoice2 < 0)
-                        {
-                            throw new IndexOutOfRangeException();
-                        }
-                        Console.WriteLine("Are you sure you want to delete this task? (y/n)");
-                        string choice = Console.ReadLine();
-                        string choice2 = choice.ToLower().Trim();
-                        if (choice2 == "y" || choice2 == "yes")
-                        {
-                            tasks.RemoveAt(userChoice2);
-                            Console.WriteLine("Task deleted successfully");
-                        }
-                        else
-                        {
-                            break;
-                        }
+                        DeleteTask(tasks, goAgain);
                     }
-
                     if (userChoice == "4" || userChoice == "Mark task complete")
                     {
-                        JobList(tasks);
-                        Console.WriteLine("Which task would you like to mark complete?");
-                        string input2 = Console.ReadLine();
-                        int input3 = Int32.Parse(input2);
-                        int userChoice2 = Int32.Parse(input2) - 1;
-                        //check if userchoice2 is in index
-                        if (userChoice2 > tasks.Count() || input3 < 1)
-                        {
-                            throw new IndexOutOfRangeException();
-                        }
-                        tasks[userChoice2].Status = true;
-                        Console.WriteLine($"\nTask {userChoice2 + 1} marked complete");
-                        JobInfo(tasks, userChoice2);
+                        MarkComplete(tasks);
                     }
                     if (userChoice == "5" || userChoice == "Edit a task")
                     {
-                        JobList(tasks);
-                        Console.WriteLine("Which task would you like to edit?");
-                        string input2 = Console.ReadLine();
-                        int userChoice2 = Int32.Parse(input2) - 1;
-                        JobInfo(tasks, userChoice2);
-                        EditMenu();
-                        string input3 = Console.ReadLine();
-                        int userEditChoice = Int32.Parse(input3);
-                        if(userEditChoice == 1)
-                        {
-                            Console.WriteLine("Please enter a new task member name:");
-                            string newMemberName = Console.ReadLine();
-                            tasks[userChoice2].Name = newMemberName;
-                            Console.WriteLine("\nName changed successfully");
-                            JobInfo(tasks, userChoice2);
-                        }
-                        if (userEditChoice == 2)
-                        {
-                            Console.WriteLine("Please enter a new task description:");
-                            string newDescription = Console.ReadLine();
-                            tasks[userChoice2].Description = newDescription;
-                            Console.WriteLine("\nDescription changed successfully");
-                            JobInfo(tasks, userChoice2);
-                        }
-                        if (userEditChoice == 3)
-                        {
-                            Console.WriteLine("Please enter a new task due date:");
-                            string newDueDate = Console.ReadLine();
-                            tasks[userChoice2].DueDate = newDueDate;
-                            Console.WriteLine("\nDue Date changed successfully");
-                            JobInfo(tasks, userChoice2);
-                        }
-                        if (userEditChoice == 4)
-                        {
-                            if(tasks[userChoice2].Status == true)
-                            {
-                                Console.WriteLine("Task is currently marked complete. Are you sure you want to mark it incomplete?");
-                                string choice = Console.ReadLine();
-                                string choice2 = choice.ToLower().Trim();
-                                if(choice2 == "y")
-                                {
-                                  tasks[userChoice2].Status= false;
-                                    Console.WriteLine("Task marked incomplete successfully");
-                                    JobInfo(tasks, userChoice2);
-                                }
-                                else if(choice2 == "n")
-                                {
-                                    break;
-                                }
-                                else
-                                {
-                                    Console.WriteLine(@"Please enter a ''y'' or an ''n'' ");
-                                    throw new FormatException();
-                                }
-                            }
-                            else if(tasks[userChoice2].Status == false)
-                            {
-                                Console.WriteLine("Task is currently marked incomplete. Are you sure you want to mark it complete?");
-                                string choice = Console.ReadLine();
-                                string choice2 = choice.ToLower().Trim();
-                                if (choice2 == "y")
-                                {
-                                    tasks[userChoice2].Status = true;
-                                    Console.WriteLine("Task marked complete successfully");
-                                    JobInfo(tasks, userChoice2);
-                                }
-                            }
-                        }
+                        EditTask(tasks, goAgain);
                     }
 
                     if (userChoice == "6" || userChoice == "Quit")
@@ -216,7 +97,7 @@ namespace TaskList
             }
 
         }
-        public static void JobList(List<Job> tasks)
+    public static void JobList(List<Job> tasks)
         {
             foreach (Job t in tasks)
             {
@@ -234,8 +115,7 @@ namespace TaskList
                 Console.WriteLine();
             }
         }
-
-        public static void JobInfo(List<Job> tasks, int userChoice2)
+    public static void JobInfo(List<Job> tasks, int userChoice2)
         {
             foreach (Job t in tasks)
             {
@@ -256,7 +136,80 @@ namespace TaskList
                 Console.WriteLine();
             }
         }
-        public static void EditMenu()
+    public static void AddTask(List<Job> tasks)
+        {
+            Console.WriteLine("Please enter the team member's name: ");
+            string taskName = Console.ReadLine();
+            Console.WriteLine("Please enter the task description: ");
+            string taskDescription = Console.ReadLine();
+            Console.WriteLine("Please enter the due date: ");
+            string dueDate = Console.ReadLine();
+            tasks.Add(new Job($"{taskName}", $"{taskDescription}", $"{dueDate}", false));
+        }
+    public static void DeleteTask(List<Job> tasks, bool goAgain)
+        {
+            foreach (Job t in tasks)
+            {
+                Console.WriteLine($"    {tasks.IndexOf(t) + 1}.Team Member: {t.Name}");
+                Console.WriteLine($"    Task Description: {t.Description}");
+                Console.WriteLine($"    Task Due Date: {t.DueDate}");
+                if (t.Status == true)
+                {
+                    Console.WriteLine("    Task Status: Complete");
+                }
+                else
+                {
+                    Console.WriteLine("    Task Status: Incomplete");
+                }
+            }
+            Console.WriteLine("Which task would you like to delete?");
+            string input2 = Console.ReadLine();
+            int userChoice2 = Int32.Parse(input2) - 1;
+            //check if userchoice2 is in index
+            if (userChoice2 > tasks.Count() || userChoice2 < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            Console.WriteLine("Are you sure you want to delete this task? (y/n)");
+            string choice = Console.ReadLine();
+            string choice2 = choice.ToLower().Trim();
+            if (choice2 == "y" || choice2 == "yes")
+            {
+                tasks.RemoveAt(userChoice2);
+                Console.WriteLine("Task deleted successfully");
+            }
+            else
+            {
+                Console.WriteLine("Ok. Would you like to return to the main menu?");
+                string goBack = Console.ReadLine().Trim().ToLower();
+                if (goBack == "y"|| goBack == "yes")
+                {
+                    goAgain = true;
+                }
+                else
+                {
+                    Console.WriteLine("Goodbye! Have a great day!");
+                    goAgain = false;
+                }
+            }
+        }
+    public static void MarkComplete(List<Job> tasks)
+        {
+            JobList(tasks);
+            Console.WriteLine("Which task would you like to mark complete?");
+            string input2 = Console.ReadLine();
+            int input3 = Int32.Parse(input2);
+            int userChoice2 = Int32.Parse(input2) - 1;
+            //check if userchoice2 is in index
+            if (userChoice2 > tasks.Count() || input3 < 1)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            tasks[userChoice2].Status = true;
+            Console.WriteLine($"\nTask {userChoice2 + 1} marked complete");
+            JobInfo(tasks, userChoice2);
+        }
+    public static void EditMenu()
         {
             Console.WriteLine("---Task Edit Menu---");
             Console.WriteLine("\n1.Edit the task member name");
@@ -266,6 +219,79 @@ namespace TaskList
             Console.WriteLine("\nPlease choose an option by entering it's number: ");
 
         }
+    public static void EditTask(List<Job> tasks, bool goAgain)
+        {
 
+            JobList(tasks);
+            Console.WriteLine("Which task would you like to edit?");
+            string input2 = Console.ReadLine();
+            int userChoice2 = Int32.Parse(input2) - 1;
+            JobInfo(tasks, userChoice2);
+            EditMenu();
+            string input3 = Console.ReadLine();
+            int userEditChoice = Int32.Parse(input3);
+            if (userEditChoice == 1)
+            {
+                Console.WriteLine("Please enter a new task member name:");
+                string newMemberName = Console.ReadLine();
+                tasks[userChoice2].Name = newMemberName;
+                Console.WriteLine("\nName changed successfully");
+                JobInfo(tasks, userChoice2);
+            }
+            if (userEditChoice == 2)
+            {
+                Console.WriteLine("Please enter a new task description:");
+                string newDescription = Console.ReadLine();
+                tasks[userChoice2].Description = newDescription;
+                Console.WriteLine("\nDescription changed successfully");
+                JobInfo(tasks, userChoice2);
+            }
+            if (userEditChoice == 3)
+            {
+                Console.WriteLine("Please enter a new task due date:");
+                string newDueDate = Console.ReadLine();
+                tasks[userChoice2].DueDate = newDueDate;
+                Console.WriteLine("\nDue Date changed successfully");
+                JobInfo(tasks, userChoice2);
+            }
+            if (userEditChoice == 4)
+            {
+                if (tasks[userChoice2].Status == true)
+                {
+                    Console.WriteLine("Task is currently marked complete. Are you sure you want to mark it incomplete?");
+                    string choice = Console.ReadLine();
+                    string choice2 = choice.ToLower().Trim();
+                    if (choice2 == "y")
+                    {
+                        tasks[userChoice2].Status = false;
+                        Console.WriteLine("Task marked incomplete successfully");
+                        JobInfo(tasks, userChoice2);
+                    }
+                    else if (choice2 == "n")
+                    {
+                        Console.WriteLine("Goodbye! Have a nice day!");
+                        goAgain = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine(@"Please enter a ''y'' or an ''n'' ");
+                        throw new FormatException();
+                    }
+                }
+                else if (tasks[userChoice2].Status == false)
+                {
+                    Console.WriteLine("Task is currently marked incomplete. Are you sure you want to mark it complete?");
+                    string choice = Console.ReadLine();
+                    string choice2 = choice.ToLower().Trim();
+                    if (choice2 == "y")
+                    {
+                        tasks[userChoice2].Status = true;
+                        Console.WriteLine("Task marked complete successfully");
+                        JobInfo(tasks, userChoice2);
+                    }
+                }
+            }
+        }
     }
+
 }
